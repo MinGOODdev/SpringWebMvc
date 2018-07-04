3b4c646b909ecb1441bdc19b92d79363e4bf0d2e
mingooddev <mingood92@gmail.com>
2018-07-04 18:56:06 +0900

    Test: List Paging
    Ver 1보다 더 단순화한 테스트

--- src/test/java/dao/ArticleDaoTest.java
+++ src/test/java/dao/ArticleDaoTest.java
@@ -2,6 +2,7 @@ package dao;
 
 import com.spring.article.dao.ArticleDao;
 import com.spring.article.dto.Article;
+import com.spring.commons.paging.Criteria;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -54,6 +55,35 @@ public class ArticleDaoTest {
     List<Article> articles = articleDao.findAllArticle();
     for (Article a : articles) {
       logger.info("title: {} content: {}", a.getTitle(), a.getContent());
+    }
+  }
+
+  @Test
+  public void testFindAllArticlePaging() throws Exception {
+    Criteria criteria = new Criteria();
+    criteria.setPage(3);
+    criteria.setPerPageNum(20);
+
+    List<Article> articles = articleDao.findAllArticlePaging(criteria);
+
+    for (Article article : articles) {
+      logger.info(article.getArticleNo() + ": " + article.getTitle());
+    }
+  }
+
+  /**
+   * 더미 데이터를 삽입하는 코드
+   *
+   * @throws Exception
+   */
+  @Test
+  public void createDummyData() throws Exception {
+    for (int i = 0; i < 1000; ++i) {
+      Article article = new Article();
+      article.setTitle(i + "번째 title");
+      article.setContent(i + "번째 content");
+      article.setWriter("user" + i);
+      articleDao.createArticle(article);
     }
   }
 
